e2c62a72046009abcd9ed5c8805f6dea7964129d
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-08 21:13:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -34,7 +34,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
   holiday_hash[:winter].each do |holiday, decorations|
-    decorations.push(item)
+    decorations.push(supply)
    end
 end
 
