4bb5185c1227695de88d758630bc045a6d6f3d0a
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-08 21:06:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,7 +45,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-holiday_supplies.values
+holiday_supplies[:winter].values
 end
 
 def all_supplies_in_holidays(holiday_hash)
